40109bcb0a4db2adb06043386eda845a8a0416d7
liuyanjuntr-ltarctfp <3186335192@qq.com>
2020-05-05 00:40:21 +0800

    web-dm

--- node-1/js/module01.js
+++
@@ -1,9 +0,0 @@
-function hi() {
-    console.log("hi module01.js")
-}
-/* module.exports = hi();
-module.exports.a = 'abc'; */
-module.exports = {
-    hi: hi,
-    abc: 'abc'
-}
--- node-1/node-01.js
+++
@@ -1,2 +0,0 @@
-console.log("hello world");
-console.log("疫情快点结束，我想上班！");
